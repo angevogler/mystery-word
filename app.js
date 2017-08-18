c54cc820f42fe31f17d6f787be96514769503c2c
Angela Vogler <angelavogler4@gmail.com>
2017-08-18 16:19:38 -0400

    got letters to show up in blanks in console

--- app.js
+++ app.js
@@ -18,10 +18,86 @@ app.engine('mustache', mustache());
 app.set('views', './views')
 app.set('view engine', 'mustache');
 
-// add variable for dictionary
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+let randomWord = null;
+let word = [];
+let blanks = [];
+let display;
+let lettersGuessed = [];
+
+// when a user that is not in a current game arrives at your root page
+app.get('/', function (req, res) {
+  // blanks = blanks;
+  console.log(blanks);
+  // store the word in a session
+  req.session.word = word;
+
+  // display the # of letters (_ _ _ _) & guessing form
+  res.render('game', {
+    word: blanks,
+    lettersGuessed: lettersGuessed,
+  });
+});
+
+// when a user supplies a guess
+app.post('/guess', function (req, res) {
+  let guess = req.body.guess;
+  // validate submitted form to make sure there is only one letter
+  if (guess.length === 1) {
+    lettersGuessed.push(guess);
+    console.log(lettersGuessed);
+    for (let i = 0; i < word.length; i++) {
+      let letter = word[i];
+      // if the guess matches a letter in the word
+      if (guess === word[i]) {
+        // display that correct letter
+        blanks[i] = guess;
+      }
+    }
+    res.redirect('/');
+  // if user enters more than one letter, display input invalid msg and let them try again
+  } else {
+    res.render('game', {
+      word: blanks,
+      lettersGuessed,
+      error: true,
+    });
+  }
+
+  // display if guess was correct or incorrect
+  // for (let i = 0; i < word.length; i++) {
+  //   let letter = word[i];
+  //   // if the guess matches a letter in the word
+  //   if (guess === word[i]) {
+  //     // display that correct letter
+  //     blanks[i] = guess;
+  //   }
+  // }
+  // display partially guessed word + letter spaces that have not been guessed
+  // remind user of guesses left
+    // guesses left are determined by what is in the session
+    // lose a guess when guess is incorrect
+    // if user guesses same letter twice do not take away guess but display error message
+
+  // game ends when user constructs full word or runs out of guesses
+    // if player constructs full word display message
+        // let stillBlank = false;
+    // if player runs out of guesses, reveal correct word
+    // when game ends, ask the user if they want to play again
+});
 
 // run the server
 app.listen(4000, function () {
-    console.log('Let the games begin');
+  // create function to get random word
+  // words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+  randomWord = words[Math.floor(Math.random() * words.length)];
+  // push that word into an array
+  word = randomWord.split('');
+  for (let i = 0; i < word.length; i++) {
+    blanks.push('_');
+  }
+  blanks = blanks;
+
+  console.log('Let the games begin');
+  console.log(randomWord);
 });
